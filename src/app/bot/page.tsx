ebf5cab66b5d211044277bfaee2a142998ea2f02
owbird <owbirdphorccuo@gmail.com>
2024-11-09 20:48:13 +0000

    feat: Add bot description

---
+++ src/app/bot/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <div className="max-w-3xl h-screen mx-auto p-6 text-gray-800">
+      <section>
+        <h2 className="text-2xl font-semibold text-green-500 mb-4">
+          About the DaXSome-Wizard
+        </h2>
+        <p >
+          The DaXSome-Wizard is an automated tool developed to gather publicly
+          available information from the web to enhance the quality and breadth
+          of our datasets. Our bot enables efficient data collection to support
+          research, data science, AI model training, and business analytics
+          across various sectors. The DaXSome-Wizard operates within
+          established guidelines to ensure responsible data collection and
+          respect for website owners and users.
+        </p>
+      </section>
+    </div>
+  );
+}
