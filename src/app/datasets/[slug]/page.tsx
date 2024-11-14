8e8f27e2f4a76ae375da5c916b0cff5a1af02c83
owbird <owbirdphorccuo@gmail.com>
2024-11-14 08:38:22 +0000

    feat (dataset): Add dataset meta tags

--- src/app/datasets/[slug]/page.tsx
+++ src/app/datasets/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { DatasetView } from "@/components/dataset-page";
 import { HOST_URL } from "@/utils";
+import { Metadata } from "next";
 
 interface Props {
   params: Promise<{ slug: string }>;
@@ -7,12 +8,28 @@ interface Props {
 
 export const revalidate = 86400; //A Day
 
+export const generateMetadata = async ({ params }: Props) => {
+  const { slug } = await params;
+
+  const res = await fetch(`${HOST_URL}/api/datasets/${slug}`);
+
+  const dataset = await res.json();
+
+  const data: Metadata = {
+    metadataBase: new URL(HOST_URL),
+    title: dataset.title,
+    description: dataset.description,
+  };
+
+  return data;
+};
+
 export default async function Page({ params }: Props) {
   const { slug } = await params;
 
   const res = await fetch(`${HOST_URL}/api/datasets/${slug}`);
 
-  const dataset  = (await res.json())
+  const dataset = await res.json();
 
   return <DatasetView dataset={dataset} />;
 }
