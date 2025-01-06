e873ff5e6e516061e66d0a4a068489787f103c04
Owbird <owbirdphorccuo@gmail.com>
2025-01-06 22:37:04 +0000

    feat: Generate sitemap on build

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://daxsome.org",
+  generateRobotsTxt: true,
+  exclude: ["/api/*"]
+};
--- src/app/datasets/[slug]/page.tsx
+++ src/app/datasets/[slug]/page.tsx
@@ -1,7 +1,7 @@
+import { Dataset } from "@/backend/models/datasets";
 import { DatasetView } from "@/components/dataset-page";
 import { DatasetInfo } from "@/types";
-import { HOST_URL } from "@/utils";
-import { Metadata } from "next";
+import { HOST_URL, parseDatasetSlug } from "@/utils";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -17,14 +17,24 @@ export const generateMetadata = async ({ params }: Props) => {
 
   const dataset = await res.json();
 
-  const data: Metadata = {
+  return {
     metadataBase: new URL(HOST_URL),
     title: dataset.title,
     description: dataset.description,
   };
-
-  return data;
 };
+
+export async function generateStaticParams() {
+  const res = await fetch(`${HOST_URL}/api/datasets`);
+
+  const { datasets } = await res.json();
+
+  const paths = (datasets as Dataset[]).map((dataset) => ({
+    slug: parseDatasetSlug(dataset.name),
+  }));
+
+  return paths;
+}
 
 export default async function Page({ params }: Props) {
   const { slug } = await params;
@@ -34,7 +44,7 @@ export default async function Page({ params }: Props) {
   const dataset = await res.json() as DatasetInfo
 
   if (res.status === 404 || dataset.status === "pending") {
-    return  notFound()
+    return notFound();
   }
 
   return <DatasetView dataset={dataset} />;
