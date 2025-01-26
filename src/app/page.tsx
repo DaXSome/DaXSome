631d46cb6d88024ba5274ce2736779b64a220928
Owbird <owbirdphorccuo@gmail.com>
2025-01-26 02:34:48 +0000

    feat (landing-page): Add who we serve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Hero from "@/components/landing-page/Hero";
+import WhoWeServe from "@/components/landing-page/Who-We-Serve";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <main className="flex-1">
-
-<Hero />
-
-
+        <Hero />
+        <WhoWeServe />
       </main>
     </div>
   );
