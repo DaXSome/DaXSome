eee4156c2efdea10970c16250afbf3f7905da9ec
Owbird <owbirdphorccuo@gmail.com>
2025-02-03 11:03:06 +0000

    fix (dataset-card): Fix missing types

--- src/components/datasets/DatasetCard.tsx
+++ src/components/datasets/DatasetCard.tsx
@@ -1,60 +1,68 @@
-import { parseDatasetSlug } from "@/utils";
+import { parseDatasetSlug } from '@/utils';
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { DatasetInfo } from "@/types";
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { DatasetInfo } from '@/types';
 
 const DatasetCard = ({ dataset }: { dataset: DatasetInfo }) => {
-  const isPublished = dataset.status === "published";
+    const metadata = dataset.metadata!;
 
-  return (
-    <Link
-      key={dataset._id}
-      href={
-        !isPublished
-          ? "#"
-          : `/datasets/@${dataset.user.username}/${parseDatasetSlug(dataset.name)}`
-      }
-      className="h-full"
-    >
-      <Card className="border-primary h-full flex flex-col">
-        <CardHeader>
-          <CardTitle className="line-clamp-2">{dataset.name}</CardTitle>
-          <CardDescription>{dataset.category}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex-grow">
-          <p className="mb-4 line-clamp-3">{dataset.description}</p>
-          <div className="flex flex-wrap gap-2">
-            {dataset.tags.map((tag) => (
-              <Badge key={tag} className="text-white">
-                {tag}
-              </Badge>
-            ))}
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-between mt-auto">
-          {!isPublished && <Badge variant={"destructive"}>Coming Soon</Badge>}
+    const isPublished = metadata?.status === 'published';
 
-          <div className="flex gap-2 items-center">
-            <Avatar>
-              <AvatarImage src={dataset.user.avatar} />
-              <AvatarFallback>{dataset.user.username}</AvatarFallback>
-            </Avatar>
+    return (
+        <Link
+            key={dataset._id}
+            href={
+                !isPublished
+                    ? '#'
+                    : `/datasets/@${dataset.user.username}/${parseDatasetSlug(dataset.name)}`
+            }
+            className="h-full"
+        >
+            <Card className="border-primary h-full flex flex-col">
+                <CardHeader>
+                    <CardTitle className="line-clamp-2">
+                        {dataset.name}
+                    </CardTitle>
+                    <CardDescription>{metadata.category}</CardDescription>
+                </CardHeader>
+                <CardContent className="flex-grow">
+                    <p className="mb-4 line-clamp-3">{metadata.description}</p>
+                    <div className="flex flex-wrap gap-2">
+                        {dataset.metadata?.tags.map((tag) => (
+                            <Badge key={tag} className="text-white">
+                                {tag}
+                            </Badge>
+                        ))}
+                    </div>
+                </CardContent>
+                <CardFooter className="flex justify-between mt-auto">
+                    {!isPublished && (
+                        <Badge variant={'destructive'}>Coming Soon</Badge>
+                    )}
 
-            <p>@{dataset.user.username}</p>
-          </div>
-        </CardFooter>
-      </Card>
-    </Link>
-  );
+                    <div className="flex gap-2 items-center">
+                        <Avatar>
+                            <AvatarImage src={dataset.user.avatar} />
+                            <AvatarFallback>
+                                {dataset.user.username}
+                            </AvatarFallback>
+                        </Avatar>
+
+                        <p>@{dataset.user.username}</p>
+                    </div>
+                </CardFooter>
+            </Card>
+        </Link>
+    );
 };
 
 export default DatasetCard;
