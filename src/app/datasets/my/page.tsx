8f473a14052ce60a0958620085ad0d9bb75ab4cf
Owbird <owbirdphorccuo@gmail.com>
2025-02-17 18:24:09 +0000

    fix (datasets): Fix layout

--- src/app/datasets/my/page.tsx
+++ src/app/datasets/my/page.tsx
@@ -10,7 +10,7 @@ const Page = async () => {
     if (!databases) return notFound();
 
     return (
-        <div className="px-4 py-8 flex-1">
+        <div className="h-screen px-4 py-8 flex-1">
             <div className="w-full mx-auto px-24 flex flex-col justify-center place-items-center">
                 <div className="h-[450px] flex flex-col gap-3 w-full place-items-center">
                     <h1 className="flex text-3xl font-extrabold text-gray-800 mb-6 text-center items-center gap-4">
