6115655ff6febb2f063ad62345fc4ff8c116f18b
Owbird <owbirdphorccuo@gmail.com>
2025-03-09 21:25:51 +0000

    fix (datasets): Fix missing suspense error

--- src/app/datasets/page.tsx
+++ src/app/datasets/page.tsx
@@ -1,18 +1,22 @@
-import Datasets from "@/components/datasets/Datasets";
-import { HOST_URL } from "@/utils";
-import { Metadata } from "next";
+import Datasets from '@/components/datasets/Datasets';
+import { HOST_URL } from '@/utils';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const generateMetadata = async () => {
+    const data: Metadata = {
+        metadataBase: new URL(HOST_URL),
+        title: `DaXSome -  Datasets`,
+        description: `Explore Datasets on DaXSome`,
+    };
 
-  const data: Metadata = {
-    metadataBase: new URL(HOST_URL),
-    title: `DaXSome -  Datasets`,
-    description: `Explore Datasets on DaXSome`,
-  };
-
-  return data;
+    return data;
 };
 
 export default async function Page() {
-  return <Datasets />;
+    return (
+        <Suspense>
+            <Datasets />;
+        </Suspense>
+    );
 }
